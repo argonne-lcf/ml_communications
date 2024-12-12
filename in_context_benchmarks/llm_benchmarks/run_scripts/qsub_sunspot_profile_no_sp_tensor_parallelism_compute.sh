a329490af6c2848bb199209c3854976fead0daa6
Khalid Hossain <khossain4337@gmail.com>
2024-12-12 18:16:10 +0000

    Added unitrace wrapper script for sunspot.

---
+++ in_context_benchmarks/llm_benchmarks/run_scripts/qsub_sunspot_profile_no_sp_tensor_parallelism_compute.sh
@@ -0,0 +1,72 @@
+#!/bin/bash -x
+#PBS -l select=2
+#PBS -l place=scatter
+#PBS -l walltime=00:30:00
+#PBS -q workq
+#PBS -A Aurora_deployment
+#PBS -l filesystems=home:flare
+#PBS -k doe
+#PBS -e /home/hossainm/ml_communications/in_context_benchmarks/llm_benchmarks/run_scripts/errordir_sunspot
+#PBS -o /home/hossainm/ml_communications/in_context_benchmarks/llm_benchmarks/run_scripts/outdir_sunspot
+#PBS -j oe
+#PBS -N LLM_test_8x12 
+
+## Timezone US/Central
+export TZ='/usr/share/zoneinfo/US/Central'
+
+# Define a timestamp function
+timestamp() {
+  date +"%Y-%m-%d %H:%M:%S" # current time
+}
+
+echo "$(timestamp): Start of the Run, after exporting TZ Central"
+
+WORK_DIR=/home/hossainm/ml_communications/in_context_benchmarks/llm_benchmarks
+LOG_WRAPPER=${WORK_DIR}/log_wrapper.sh 
+
+TP_DEGREE=12
+WARMUPS=4
+TIMING_LOOPS=4
+PRECISION="float32"
+N_LAYERS=1
+TRIAL=1
+
+# MPI and OpenMP settings
+NNODES=`wc -l < $PBS_NODEFILE`
+NRANKS_PER_NODE=12
+
+let NRANKS=${NNODES}*${NRANKS_PER_NODE}
+
+module load frameworks/2024.2.1_u1
+
+#export CPU_AFFINITY="verbose,list:0-7,104-111:8-15,112-119:16-23,120-127:24-31,128-135:32-39,136-143:40-47,144-151:52-59,156-163:60-67,164-171:68-75,172-179:76-83,180-187:84-91,188-195:92-99,196-203"
+export CPU_AFFINITY="list:0-2,4-7,104-111:8-10,12-15,112-119:16-18,20-23,120-127:24-26,28-31,128-135:32-34,36-39,136-143:40-42,44-47,144-151:52-54,56-59,156-163:60-62,64-67,164-171:68-70,72-75,172-179:76-78,80-83,180-187:84-86,88-91,188-195:92-94,96-99,196-203"
+#export CPU_AFFINITY="verbose,list:2-4:10-12:18-20:26-28:34-36:42-44:54-56:62-64:70-72:78-80:86-88:94-96"
+export HOROVOD_THREAD_AFFINITY="4,12,20,28,36,44,56,64,72,80,88,96"
+#export CCL_WORKER_AFFINITY="5,13,21,29,37,45,57,65,73,81,89,97"
+export CCL_WORKER_AFFINITY="3,11,19,27,35,43,55,63,71,79,87,95"
+export MEM_BIND="list:2:2:2:2:2:2:3:3:3:3:3:3"
+
+echo "========= ENVIRONMENT VARIABLES ======="
+env
+echo "========= ENVIRONMENT VARIABLES ======="
+
+echo ""
+
+echo "========= CCL VARIABLES =============="
+printenv | grep "CCL"
+echo "========= CCL VARIABLES =============="
+
+RUN_ID=sunspot_profile_ten_par_TP${TP_DEGREE}_NO_SP_L${N_LAYERS}_TL${TIMING_LOOPS}_${PRECISION}_N${NNODES}_R${NRANKS_PER_NODE}_T${TRIAL}_$(date +"%Y-%m-%d_%H-%M-%S")
+
+echo "${RUN_ID}"
+
+
+echo "$(timestamp): Before mpiexec."
+
+mpiexec --pmi=pmix -n ${NRANKS} -ppn ${NRANKS_PER_NODE} -l --line-buffer --cpu-bind ${CPU_AFFINITY} --mem-bind ${MEM_BIND} \
+${LOG_WRAPPER} python ${WORK_DIR}/tensor_parallel_with_gradient_synchronization.py -dvc "xpu" \
+-tp_degree=${TP_DEGREE} --warmup_iterations ${WARMUPS} --iterations=${TIMING_LOOPS} --precision ${PRECISION} -n_layers ${N_LAYERS} \
+--logging --log_directory=${WORK_DIR}/run_scripts/outdir_sunspot/logs --log_file=${RUN_ID}.log --trace ${RUN_ID}
+
+echo "$(timestamp): Finished the workload."
---
+++ in_context_benchmarks/llm_benchmarks/run_scripts/unitrace_wrapper_gt4.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+## This wrapper should be used with unitrace to trace in the case of larger than
+## 4 Nodes. The script is set up to trace rank 0 of first 4 Nodes in the case of
+## profiling a job running on larger than 4 nodes.
+FNAME_EXT=$(basename "$2")
+FNAME="${FNAME_EXT%%.*}"
+ 
+NNODES=`wc -l < $PBS_NODEFILE`
+
+WORK_DIR=/home/hossainm/ml_communications/in_context_benchmarks/llm_benchmarks
+UNITRACE_DIR=/opt/aurora/24.180.1/support/tools/pti-gpu/063214e
+UNITRACE_LIB=${UNITRACE_DIR}/lib64
+UNITRACE_BIN=${UNITRACE_DIR}/bin
+UNITRACE_EXE=${UNITRACE_BIN}/unitrace
+DTAG=$(date +%F_%H%M%S)
+UNITRACE_OUTDIR=${WORK_DIR}/run_scripts/outdir_sunspot/logs/unitrace_profiles/tp_no_sp_pt_ddp_json_n${NNODES}_${DTAG}/${FNAME}_n${NNODES}_${DTAG}
+mkdir -p ${UNITRACE_OUTDIR}
+#UNITRACE_OPTS=" --chrome-sycl-logging  --chrome-mpi-logging  --chrome-ccl-logging  --chrome-dnn-logging --chrome-kernel-logging --chrome-device-logging --chrome-no-thread-on-device --chrome-no-engine-on-device --output-dir-path ${ITRACEOUTDIR}  --output ${ITRACEOUTDIR}/${FNAME}-ut "
+UNITRACE_OPTS=" --ccl-summary-report --chrome-mpi-logging --chrome-sycl-logging \
+--chrome-device-logging \
+--chrome-ccl-logging --chrome-call-logging --chrome-dnn-logging --device-timing --host-timing \
+--output-dir-path ${UNITRACE_OUTDIR} --output ${UNITRACE_OUTDIR}/UNITRACE_${FNAME}_n${NNODES}_${DTAG}.txt "
+
+ 
+export LD_LIBRARY_PATH=${UNITRACE_LIB}:${UNITRACE_BIN}:$LD_LIBRARY_PATH
+ 
+# Use $PMI_RANK for MPICH and $SLURM_PROCID with srun.
+#PROFRANK=$(( ${NNODES}-1 ))
+#PROFRANK=$(( $PALS_LOCAL_SIZE-1 ))
+PROFRANK=0
+RANKCUTOFF=48
+#if [[ $PMIX_RANK -eq $PROFRANK ]]; then
+#  echo "On rank $PMIX_RANK, collecting traces "
+#  $UNITRACE_EXE $UNITRACE_OPTS "$@"
+#else
+#  "$@"
+#fi
+if [[ $PALS_LOCAL_RANKID -eq $PROFRANK ]] && [[ $PMIX_RANK -lt $RANKCUTOFF ]]; then
+  echo "On rank $PMIX_RANK, collecting traces "
+  $UNITRACE_EXE $UNITRACE_OPTS "$@"
+else
+  "$@"
+fi
