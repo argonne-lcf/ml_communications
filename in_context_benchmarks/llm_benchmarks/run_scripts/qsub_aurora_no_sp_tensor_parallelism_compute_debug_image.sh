3318f55d3b11aff8931fc47d46a32cd1ee9d4c9c
Khalid Hossain <hossainm@aurora-uan-0011.hostmgmt.cm.aurora.alcf.anl.gov>
2025-03-04 17:17:44 +0000

    Added changed bucket size in the Aurora job script

--- in_context_benchmarks/llm_benchmarks/run_scripts/qsub_aurora_no_sp_tensor_parallelism_compute_debug_image.sh
+++ in_context_benchmarks/llm_benchmarks/run_scripts/qsub_aurora_no_sp_tensor_parallelism_compute_debug_image.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -x
-#PBS -l select=1
+#PBS -l select=2
 #PBS -l place=scatter
 #PBS -l walltime=00:20:00
 #PBS -q debug-scaling
@@ -9,7 +9,7 @@
 #PBS -e /home/hossainm/ml_communications/in_context_benchmarks/llm_benchmarks/run_scripts/errordir_aurora
 #PBS -o /home/hossainm/ml_communications/in_context_benchmarks/llm_benchmarks/run_scripts/outdir_aurora
 #PBS -j oe
-#PBS -N TP6_R6
+#PBS -N TP12_R12
 
 ## Timezone US/Central
 export TZ='/usr/share/zoneinfo/US/Central'
@@ -24,16 +24,21 @@ echo "$(timestamp): Start of the Run, after exporting TZ Central"
 WORK_DIR=/home/hossainm/ml_communications/in_context_benchmarks/llm_benchmarks
 LOG_WRAPPER=${WORK_DIR}/log_wrapper.sh 
 
-TP_DEGREE=6
+TP_DEGREE=12
 #WARMUPS=1
 TIMING_LOOPS=4
-PRECISION="float32"
-N_LAYERS=80
-TRIAL=2
+PRECISION="bfloat16"
+N_LAYERS=1
+## Special flag for deterministic input of torch ones. The defaults is 
+# torch.normal with std_dev 0.01 around mean 0.00
+IN_TYPE="random"
+#IN_TYPE="torch_ones"
+BUCKET=1e9
+TRIAL=1
 
 # MPI and OpenMP settings
 NNODES=`wc -l < $PBS_NODEFILE`
-NRANKS_PER_NODE=6
+NRANKS_PER_NODE=12
 
 let NRANKS=${NNODES}*${NRANKS_PER_NODE}
 
@@ -64,12 +69,12 @@ module load frameworks/2024.2.1_u1
 
 ## For TP=6, PPN=6, Setup 2
 # CPU cores from 2 sockets, Physical core 0-51:socket 1, Physical Core 52-103: socket 2 
-export CPU_AFFINITY="list:0-2,4-7,104-111:8-10,12-15,112-119:16-18,20-23,120-127:24-26,28-31,128-135:52-54,56-59,156-163:60-62,64-67,164-171"
-export HOROVOD_THREAD_AFFINITY="4,12,20,56,64,72"
-export CCL_WORKER_AFFINITY="3,11,19,55,63,71"
-export MEM_BIND="list:2:2:2:3:3:3"
+#export CPU_AFFINITY="list:0-2,4-7,104-111:8-10,12-15,112-119:16-18,20-23,120-127:24-26,28-31,128-135:52-54,56-59,156-163:60-62,64-67,164-171"
+#export HOROVOD_THREAD_AFFINITY="4,12,20,56,64,72"
+#export CCL_WORKER_AFFINITY="3,11,19,55,63,71"
+#export MEM_BIND="list:2:2:2:3:3:3"
 ## Asymmetric division, full GPUs used only - GPU 0, 1 and GPU 3
-export ZE_AFFINITY_MASK="0,1,2,3,6,7"
+#export ZE_AFFINITY_MASK="0,1,2,3,6,7"
 
 
 ## For TP=8,16,24 PPN=8
@@ -82,11 +87,11 @@ export ZE_AFFINITY_MASK="0,1,2,3,6,7"
 #export ZE_AFFINITY_MASK="0,1,2,3,6,7,8,9"
 
 ## For TP=12, PPN=12
-#export CPU_AFFINITY="list:0-2,4-7,104-111:8-10,12-15,112-119:16-18,20-23,120-127:24-26,28-31,128-135:32-34,36-39,136-143:40-42,44-47,144-151:52-54,56-59,156-163:60-62,64-67,164-171:68-70,72-75,172-179:76-78,80-83,180-187:84-86,88-91,188-195:92-94,96-99,196-203"
-#export HOROVOD_THREAD_AFFINITY="4,12,20,28,36,44,56,64,72,80,88,96"
-#export CCL_WORKER_AFFINITY="3,11,19,27,35,43,55,63,71,79,87,95"
-#export MEM_BIND="list:2:2:2:2:2:2:3:3:3:3:3:3"
-#export ZE_AFFINITY_MASK="0,1,2,3,4,5,6,7,8,9,10,11"
+export CPU_AFFINITY="list:0-2,4-7,104-111:8-10,12-15,112-119:16-18,20-23,120-127:24-26,28-31,128-135:32-34,36-39,136-143:40-42,44-47,144-151:52-54,56-59,156-163:60-62,64-67,164-171:68-70,72-75,172-179:76-78,80-83,180-187:84-86,88-91,188-195:92-94,96-99,196-203"
+export HOROVOD_THREAD_AFFINITY="4,12,20,28,36,44,56,64,72,80,88,96"
+export CCL_WORKER_AFFINITY="3,11,19,27,35,43,55,63,71,79,87,95"
+export MEM_BIND="list:2:2:2:2:2:2:3:3:3:3:3:3"
+export ZE_AFFINITY_MASK="0,1,2,3,4,5,6,7,8,9,10,11"
 
 
 echo "========= ENVIRONMENT VARIABLES ======="
@@ -100,11 +105,11 @@ printenv | grep "CCL"
 echo "========= CCL VARIABLES =============="
 
 ## RUN ID for PPN=6 setup, Asymmetric GPU placement
-RUN_ID=aurora_tensor_parallel_CB0816245260_ZED012367_TP${TP_DEGREE}_NO_SP_LAYERS${N_LAYERS}_TIMING_LOOPS${TIMING_LOOPS}_${PRECISION}_N${NNODES}_R${NRANKS_PER_NODE}_T${TRIAL}_$(date +"%Y-%m-%d_%H-%M-%S")
+#RUN_ID=aurora_tensor_parallel_CB0816245260_ZED012367_TP${TP_DEGREE}_NO_SP_LAYERS${N_LAYERS}_TIMING_LOOPS${TIMING_LOOPS}_${PRECISION}_N${NNODES}_R${NRANKS_PER_NODE}_T${TRIAL}_$(date +"%Y-%m-%d_%H-%M-%S")
 ## RUN ID for R8 setup, NIC balanced case
 #RUN_ID=aurora_tensor_parallel_CB08162452606876_ZED01236789_TP${TP_DEGREE}_NO_SP_LAYERS${N_LAYERS}_TIMING_LOOPS${TIMING_LOOPS}_${PRECISION}_N${NNODES}_R${NRANKS_PER_NODE}_T${TRIAL}_$(date +"%Y-%m-%d_%H-%M-%S")
 ## RUN ID for R12 setup, Regular, NIC imbalanced case
-#RUN_ID=aurora_tensor_parallel_TP${TP_DEGREE}_NO_SP_LAYERS${N_LAYERS}_TIMING_LOOPS${TIMING_LOOPS}_${PRECISION}_N${NNODES}_R${NRANKS_PER_NODE}_T${TRIAL}_$(date +"%Y-%m-%d_%H-%M-%S")
+RUN_ID=aurora_tensor_parallel_TP${TP_DEGREE}_NO_SP_L${N_LAYERS}_TL${TIMING_LOOPS}_${PRECISION}_${IN_TYPE}_ELEM_${BUCKET}_N${NNODES}_R${NRANKS_PER_NODE}_T${TRIAL}_$(date +"%Y-%m-%d_%H-%M-%S")
 
 
 echo "${RUN_ID}"
@@ -115,6 +120,7 @@ echo "$(timestamp): Before mpiexec."
 mpiexec --pmi=pmix -n ${NRANKS} -ppn ${NRANKS_PER_NODE} -l --line-buffer --cpu-bind ${CPU_AFFINITY} --mem-bind ${MEM_BIND} \
 ${LOG_WRAPPER} python ${WORK_DIR}/tensor_parallel_with_gradient_synchronization_debug.py -dvc "xpu" \
 -tp_degree=${TP_DEGREE}  --barrier --iterations=${TIMING_LOOPS} --precision ${PRECISION} -n_layers ${N_LAYERS} \
---logging --log_directory=${WORK_DIR}/run_scripts/outdir_aurora/logs/tp_sweep_for_sc25 --log_file=${RUN_ID}.log
+-bucket ${BUCKET} \
+--logging --log_directory=${WORK_DIR}/run_scripts/outdir_aurora/logs/scratch_logs --log_file=${RUN_ID}.log
 
 echo "$(timestamp): Finished the workload."
